5889203cc56d974e252c384f2811e8fd7142b3a2
Adhiraj Singh <adhirajsingh735@gmail.com>
2025-08-17 23:31:57 +0530

    Corrected

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import { AppointmentProvider } from "./contexts/AppointmentContext";
 import Appointments from "./components/patient/Appointments";
 import Schedule from "./components/doctor/Schedule";
 import HealthLogs from "./components/patient/HealthLogs";
-import Blog from "./pages/blog";
+import Blog from "./pages/Blog1";
 import Career from "./pages/Career1";
 import Notifications from "./pages/Notifications";
 

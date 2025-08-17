7af734c67f92f1bd9dd6feeefd1e9bfff276c561
SkyWalker <michalikjuraj0@gmail.com>
2025-08-17 09:59:44 +0200

    fix retrun to home by logo

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -24,6 +24,10 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
+          <a
+                key="Home"
+                href="#home"
+          >   
           <div className="flex items-center">
             <div className="w-10 h-10">
               <img
@@ -39,6 +43,7 @@ const Navbar = () => {
               CareSync
             </span>
           </div>
+          </a>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-8">

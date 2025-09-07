7edc6536394a171515b823e4158652054546456a
Adhiraj Singh <adhirajsingh735@gmail.com>
2025-09-07 16:28:39 +0530

    Error Rectified

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ptc2vtd1j1g"
+    "revision": "0.p486c375fuk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

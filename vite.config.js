e1b5f6d46c7773d3603226033ec75917fee62c7d
Adhiraj Singh <adhirajsingh735@gmail.com>
2025-09-07 16:13:30 +0530

    Config Updated

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // allow up to 5MB
         runtimeCaching: [
           {
             urlPattern: ({ url }) => url.pathname.startsWith("/api/reminders"),

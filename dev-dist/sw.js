c4ec9b6177ee579ee04bf6c10fe2626519dcb207
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-12 12:33:49 +0530

    📦 [Fixed BLOG section from scratch] Add codesync website project

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kt7r8avc2a"
+    "revision": "0.hqoc034hev"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

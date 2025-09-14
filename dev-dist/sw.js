89438f2db1769bc2315b70a97fb6d6fc281def62
SHIVANI PRAJAPATI <shivaniprajapati10jan@gmail.com>
2025-09-14 10:40:21 +0530

    animation added in logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,9 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-
-    "revision": "0.1fkmt9f2t5"
-
+    "revision": "0.n8gjabdldao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

e8e0da2492ac452d3350d0224f9d7a7a51fbe425
suresh1319 <sureshyadav6114@gmail.com>
2025-10-03 12:00:34 +0530

    free trail navigating to register'

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -77,12 +77,12 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
    * requests for URLs in the manifest.
    * See https://goo.gl/S9QRab
    */
- workbox.precacheAndRoute([{
+  workbox.precacheAndRoute([{
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jteptb8me5o"
+    "revision": "0.0i1738uiqio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
